3ab3b33ba1d10cc8a68a25c0b232ad9024228d7b
Leonard Crestez <leonard.crestez@nxp.com>
2018-10-18 20:06:41 +0300

    trace-idle-latency.sh: Add support for dummy load

--- trace-idle-latency.sh
+++ trace-idle-latency.sh
@@ -12,22 +12,39 @@ This calls trace-cmd record to generate trace data for idlatest
 == Options ==
     -h --help: Show this message.
     -s --sleep SLEEP_TIME: Duration of test (default 10)
+
+    --dummy-load: Run a dummy load
+        The dummy load runs many small per-cpu sleeps instead of single sleep
 MSG
+}
+
+die()
+{
+    echo "$@" >&2
+    exit 1
 }
 
 parse_args()
 {
+    orig_argv=("$@")
+
     opt_sleep=10
+    opt_mode=record
+    opt_use_load=1
 
     eval set -- $(getopt -n trace-idle-latency.sh \
-        -o -hs: -l 'help,sleep:' \
+        -o -hs: -l 'help,sleep:,mode:,dummy-load,no-dummy-load,' \
         -- "$@")
 
     while [ $# -gt 0 ]; do
         case "$1" in
         -h|--help) usage; exit 1;;
-        --sleep) opt_sleep=$2; shift;;
-        *) opt_sleep=$1 ;;
+        -s|--sleep) opt_sleep=$2; shift;;
+        --mode) opt_mode=$2; shift;;
+        --dummy-load) opt_use_load=1 ;;
+        --no-dummy-load) opt_use_load=0 ;;
+        --) break ;;
+        *) opt_sleep=$1;;
         esac
         shift
     done
@@ -40,10 +57,43 @@ trace_if_avail()
     fi
 }
 
+main_load_worker()
+{
+    set +x
+    while true; do
+        sleep 0.2
+    done
+}
+
+main_load_master()
+{
+    ncpus=$(grep processor /proc/cpuinfo | wc -l)
+    worker_pids=()
+    for cpu in `seq 0 $((ncpus - 1))`; do
+        taskset -c $cpu "$0" "${orig_argv[@]}" --mode load-worker &
+        worker_pids+=($!)
+    done
+    sleep $opt_sleep
+    kill ${worker_pids[@]}
+}
+
 main()
 {
     parse_args "$@"
 
+    if [[ $opt_mode == load-master ]]; then
+        main_load_master
+    elif [[ $opt_mode == load-worker ]]; then
+        main_load_worker
+    elif [[ $opt_mode == record ]]; then
+        main_record
+    else
+        die "Unknown mode $opt_mode"
+    fi
+}
+
+main_record()
+{
     # list events
     event_list=()
     if grep -q 'ipi:.*' /sys/kernel/debug/tracing/available_events; then
@@ -64,7 +114,11 @@ main()
     for ev in "${event_list[@]}"; do
         cmd+=(-e "$ev")
     done
-    cmd+=(sleep "$opt_sleep")
+    if [[ $opt_use_load ]]; then
+        cmd+=("$0" "${orig_argv[@]}" --mode load-master)
+    else
+        cmd+=(sleep "$opt_sleep")
+    fi
     exec "${cmd[@]}"
 }
 
