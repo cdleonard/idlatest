90ae111a7256b1226530ae9c30380f152120ec39
Leonard Crestez <leonard.crestez@nxp.com>
2018-10-18 18:56:37 +0300

    trace-idle-latency.sh: Add argument parsing

--- trace-idle-latency.sh
+++ trace-idle-latency.sh
@@ -1,7 +1,37 @@
 #! /bin/bash
 
 set -e
-set -x
+
+usage()
+{
+    cat >>/dev/stderr<<'MSG'
+trace-idle-latency.sh - Collect data for idlatest
+
+This calls trace-cmd record to generate trace data for idlatest
+
+== Options ==
+    -h --help: Show this message.
+    -s --sleep SLEEP_TIME: Duration of test (default 10)
+MSG
+}
+
+parse_args()
+{
+    opt_sleep=10
+
+    eval set -- $(getopt -n trace-idle-latency.sh \
+        -o -hs: -l 'help,sleep:' \
+        -- "$@")
+
+    while [ $# -gt 0 ]; do
+        case "$1" in
+        -h|--help) usage; exit 1;;
+        --sleep) opt_sleep=$2; shift;;
+        *) opt_sleep=$1 ;;
+        esac
+        shift
+    done
+}
 
 trace_if_avail()
 {
@@ -12,7 +42,7 @@ trace_if_avail()
 
 main()
 {
-    opt_sleep="${1:-10}"
+    parse_args "$@"
 
     # list events
     event_list=()
